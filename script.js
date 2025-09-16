05f2906406186c89dfe9b6e84eafadd853226634
mttespineli <2023-1-02218@lpu.edu.ph>
2025-09-16 11:38:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -73,6 +73,24 @@ clickImage.addEventListener('click', () => {
     clickCountSpan.textContent = clickCount;
 });
 
+const filterButtons = document.querySelectorAll('.filter-buttons button');
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+filterButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const filter = button.id.replace('Btn', '');
+
+        galleryItems.forEach(item => {
+            if (filter === 'all' || item.dataset.color === filter) {
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none';
+            }
+        });
+    });
+});
+
+
 
 
 
